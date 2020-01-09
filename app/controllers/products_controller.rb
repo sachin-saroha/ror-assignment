ed0ea93a0b4225af4109ee1487b4114b7573b373
gaurav-chaurasia <sksaroha1212@gmail.com>
2020-01-09 19:23:47 +0530

    some improvement

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -71,6 +71,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:Item_id, :Cost, :Purpose)
+      params.require(:product).permit(:Item_id, :Cost, :Purpose, :outofstock)
     end
 end
